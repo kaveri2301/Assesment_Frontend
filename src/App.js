4302351960daa47bf5b8e57909a8643c85bd5b2c
unknown <kaverikardile@gmail.com>
2024-01-26 18:34:44 +0530

    Frontend

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+// import './App.css'; 
 
-function App() {
+import React from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+// import 'bootstrap/dist/js/bootstrap.min.js'
+import {  Route, Routes } from 'react-router-dom';
+import Header from './Components/Header';
+import Login from './Pages/Login';
+import Registration from './Pages/Registration';
+import Createaccount from './Pages/Createaccount';
+import PasswordRecovery from './Pages/PasswordRecovery';
+import UsersTable from './Pages/UsersTable';
+import UpdateForm from './Pages/UpdateForm';
+import Home from './Pages/Home';
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    {/* <Header/> */}
+    
+    
+    <Routes>
+    <Route path='/' element={<Home/>}/>
+    <Route path='/login' element={<Login/>}/>
+    <Route path='/Createaccount' element={<Createaccount/>}/>
+    <Route path='/pass' element={<PasswordRecovery/>}/>
+      <Route path='/register' element={<Registration/>}/>
+      <Route path='/table' element={<UsersTable/>}/>
+      <Route path='/updateform' element={<UpdateForm/>}/>
+
+    </Routes>
+    
+    
+    
+    
+    
+    </>
+  )
 }
 
-export default App;
+
+
+
+
+
